30b7a6a9030657c259a6503de403f6b51da3beff
Alexander Gnatenko <agnatenko@gmail.com>
2020-12-12 17:04:21 +0300

    Реализованы классы MyNumber, MyStreamNumber

---
+++ src/ru/idcore/javacore/task030201/Main.java
@@ -0,0 +1,24 @@
+package ru.idcore.javacore.task030201;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        MyNumber myNumber = new MyNumber();
+        MyStreamNumber myStreamNumber = new MyStreamNumber();
+
+        List<Integer> list = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
+        //без испоьзования без использования Collection API и Stream API
+        List<Integer> list1 = myNumber.positiveEvenAscending(list);
+        System.out.println(list1.toString());
+
+        //с использованием SreamAPI
+        List<Integer> list2 = myStreamNumber.positiveEvenAscending(list);
+        System.out.println(list2.toString());
+
+
+    }
+}
---
+++ src/ru/idcore/javacore/task030201/MyStreamNumber.java
@@ -0,0 +1,23 @@
+package ru.idcore.javacore.task030201;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MyStreamNumber {
+
+    public List<Integer> positiveEvenAscending(List<Integer> list) {
+        List<Integer> integerList = new ArrayList<>();
+        Stream<Integer> stream = list.stream();
+
+        integerList = stream
+                .filter(integer -> integer > 0)
+                .filter(integer -> integer%2 == 0)
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.toList());
+        return integerList;
+    }
+}
