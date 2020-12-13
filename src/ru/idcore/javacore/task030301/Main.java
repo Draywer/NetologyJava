f2ac7cd5124801d6673855c6e596f6b197ef7a4a
Alexander Gnatenko <agnatenko@gmail.com>
2020-12-13 16:07:58 +0300

    Редвктирование файла README.md изменение пути для сохранения игры

--- src/ru/idcore/javacore/task030301/Main.java
+++ src/ru/idcore/javacore/task030301/Main.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         StringBuilder tmp = new StringBuilder();
-        String dirGame = "game";
+        String dirGame = "C://game";
         String dirSrc = dirGame + "/src";
         String dirRes = dirGame + "/res";
         String dirSavegames = dirGame + "/savegames";
