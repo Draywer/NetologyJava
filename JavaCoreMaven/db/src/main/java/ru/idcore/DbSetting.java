efa08e49e42612f2b908df8fa069d6fa4ffb659b
Alexander Gnatenko <agnatenko@gmail.com>
2020-12-22 02:48:25 +0300

    Добавлены классы в модулиA

---
+++ JavaCoreMaven/db/src/main/java/ru/idcore/DbSetting.java
@@ -0,0 +1,11 @@
+package ru.idcore;
+
+public class DbSetting {
+    private String name;
+    private String password;
+
+    public DbSetting(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
